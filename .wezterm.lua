d38f98c788ca4bef76fa317de14be551c7d532fb
Nicholas Allen <nick@bitwig.com>
2025-12-17 22:34:06 +0100

    Add wezterm config file

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font_size = 11
+config.color_scheme = "AdventureTime"
+-- config.line_height = 0.99
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+return config
