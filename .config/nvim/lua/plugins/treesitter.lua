bf75fb57d40fc680eccea8168510bae0ca0f9af6
Nicholas Allen <radicalnick@gmail.com>
2026-01-30 00:29:28 +0100

    Use enter and backspace for incremental tree sitter node selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,6 +16,16 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<Enter>',
+          node_incremental = '<Enter>',
+          scope_incremental = 'grc',
+          node_decremental = '<Backspace>',
+        },
+      },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
