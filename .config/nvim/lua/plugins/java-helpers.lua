6c7714aaf2c33433adf2b8e83c7eabc4f62bcb38
Nicholas Allen <radicalnick@gmail.com>
2026-02-08 18:33:57 +0100

    Add keybinding for going to java stack trace

--- .config/nvim/lua/plugins/java-helpers.lua
+++ .config/nvim/lua/plugins/java-helpers.lua
@@ -1,8 +1,8 @@
 return {
   {
     'NickJAllen/java-helpers.nvim',
-    -- dev = true,
-    cmd = 'JavaHelpersNewFile',
+    dev = true,
+    cmd = { 'JavaHelpersNewFile', 'JavaHelpersGoToStackTrace' },
     opts = {},
     keys = {
       { '<leader>Jn', ':JavaHelpersNewFile<cr>', desc = 'New Java Type' },
@@ -11,6 +11,7 @@ return {
       { '<leader>Ja', ':JavaHelpersNewFile Abstract Class<cr>', desc = 'New Abstract Java Class' },
       { '<leader>Jr', ':JavaHelpersNewFile Record<cr>', desc = 'New Java Record' },
       { '<leader>Je', ':JavaHelpersNewFile Enum<cr>', desc = 'New Java Enum' },
+      { '<leader>Js', ':JavaHelpersGoToStackTrace<cr>', desc = 'Go to Java stack trace line' },
     },
     dependencies = {
       { 'nvim-lua/plenary.nvim' },
