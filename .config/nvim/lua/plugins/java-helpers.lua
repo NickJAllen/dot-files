9997751f7cece30cdee9221ff947f86656d30cd5
Nicholas Allen <nick@bitwig.com>
2026-02-15 13:44:34 +0100

    Better key navigation for Java stack traces

--- .config/nvim/lua/plugins/java-helpers.lua
+++ .config/nvim/lua/plugins/java-helpers.lua
@@ -14,18 +14,23 @@ return {
     },
     opts = {},
     keys = {
+      -- New file creation
       { '<leader>Jn', ':JavaHelpersNewFile<cr>', desc = 'New Java Type' },
       { '<leader>Jc', ':JavaHelpersNewFile Class<cr>', desc = 'New Java Class' },
       { '<leader>Ji', ':JavaHelpersNewFile Interface<cr>', desc = 'New Java Interface' },
       { '<leader>Ja', ':JavaHelpersNewFile Abstract Class<cr>', desc = 'New Abstract Java Class' },
       { '<leader>Jr', ':JavaHelpersNewFile Record<cr>', desc = 'New Java Record' },
       { '<leader>Je', ':JavaHelpersNewFile Enum<cr>', desc = 'New Java Enum' },
-      { '<leader>Js', ':JavaHelpersGoToStackTraceLine<cr>', desc = 'Go to Java stack trace line' },
-      { '<leader>sj', ':JavaHelpersPickStackTraceLine<cr>', desc = 'Pick Java stack trace line' },
+
+      -- Stack trace navigation
+      { '<leader>Jg', ':JavaHelpersGoToStackTraceLine<cr>', desc = 'Go to Java stack trace line' },
+      { '<leader>JG', ':JavaHelpersGoToStackTraceLine +<cr>', desc = 'Go to Java stack trace line on Clipboard' },
+      { '<leader>Jp', ':JavaHelpersPickStackTraceLine<cr>', desc = 'Pick Java stack trace line' },
+      { '<leader>JP', ':JavaHelpersPickStackTraceLine +<cr>', desc = 'Pick Java stack trace line from Clipboard' },
       { '[j', ':JavaHelpersGoUpStackTrace<cr>', desc = 'Go up Java stack trace' },
       { ']j', ':JavaHelpersGoDownStackTrace<cr>', desc = 'Go down Java stack trace' },
-      { '<leader>[j', ':JavaHelpersGoToTopOfStackTrace<cr>', desc = 'Go to top of Java stack trace' },
-      { '<leader>]j', ':JavaHelpersGoToBottomOfStackTrace<cr>', desc = 'Go to bottom of Java stack trace' },
+      { '<leader>Jt', ':JavaHelpersGoToTopOfStackTrace<cr>', desc = 'Go to top of Java stack trace' },
+      { '<leader>Jb', ':JavaHelpersGoToBottomOfStackTrace<cr>', desc = 'Go to bottom of Java stack trace' },
       { '<leader>Jq', ':JavaHelpersSendStackTraceToQuickfix<cr>', desc = 'Send Java stack trace to quickfix list' },
     },
     dependencies = {
