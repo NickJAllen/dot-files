402b86629922899f997acc37b9afbc870a2d1b9d
Nicholas Allen <nick@bitwig.com>
2026-02-21 16:51:51 +0100

    Add harpoon plugin

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,53 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('harpoon'):setup()
+  end,
+  keys = {
+    {
+      '<leader>ha',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'Add file to harpoon',
+    },
+    {
+      '<leader><leader>',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'Show harpoon list',
+    },
+    {
+      '<leader>1',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Select file 1 in harpoon',
+    },
+    {
+      '<leader>2',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Select file 2 in harpoon',
+    },
+    {
+      '<leader>3',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Select file 3 in harpoon',
+    },
+    {
+      '<leader>4',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Select file 4 in harpoon',
+    },
+  },
+}
