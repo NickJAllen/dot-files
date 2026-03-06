1fded5ca7b8da1bf6059a80ab80bed5b835b80b7
Nicholas Allen <nick@bitwig.com>
2026-03-06 07:27:52 +0100

    New tmux splits and jj popup use neovim current file's directory

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -43,3 +43,46 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     nick.utils.jj_snapshot()
   end,
 })
+
+local last_sent_tmux_pane_dir = ''
+
+local function set_tmux_pane_directory(directory)
+  if #directory == 0 or not vim.loop.fs_stat(directory) then
+    return
+  end
+
+  if directory == last_sent_tmux_pane_dir then
+    return
+  end
+
+  last_sent_tmux_pane_dir = directory
+
+  -- print('setting tmux nvim path to ' .. directory)
+  vim.fn.jobstart { 'tmux', 'set-option', '-p', '@neovim_path', directory }
+end
+
+local function update_tmux_pane_directory()
+  local buftype = vim.bo.buftype
+  local path = vim.api.nvim_buf_get_name(0)
+
+  if #path == 0 then
+    return
+  end
+
+  local is_normal_buffer = buftype == ''
+
+  if not is_normal_buffer then
+    return
+  end
+
+  local dir = vim.fs.dirname(vim.fs.abspath(path))
+
+  set_tmux_pane_directory(dir)
+end
+
+if vim.env.TMUX then
+  vim.api.nvim_create_autocmd({ 'BufEnter', 'DirChanged' }, {
+    group = vim.api.nvim_create_augroup('TmuxPaneDirUpdate', { clear = true }),
+    callback = update_tmux_pane_directory,
+  })
+end
