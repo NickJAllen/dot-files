eabf4c5bb6f41bb29bcbe3f913b4244bfed3063f
Nicholas Allen <nick@bitwig.com>
2026-03-07 21:27:25 +0100

    Work on improving the lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,103 @@
 -- LSP Plugins
 local nick = require 'nick'
 
+local function on_lsp_attach(event)
+  local function map(keys, func, desc, mode)
+    mode = mode or 'n'
+    vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+  end
+
+  local refactor = function(func)
+    return function()
+      nick.utils.before_refactoring()
+      func()
+    end
+  end
+
+  map('<leader>cr', refactor(vim.lsp.buf.rename), 'Rename')
+
+  map('<leader>ca', refactor(vim.lsp.buf.code_action), 'Code Action', { 'n', 'x' })
+
+  map('<leader>ce', vim.diagnostic.open_float, 'Show full diagnostic error message')
+
+  local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+  if not client then
+    return
+  end
+
+  if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+    local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      buffer = event.buf,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.document_highlight,
+    })
+
+    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      buffer = event.buf,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.clear_references,
+    })
+
+    vim.api.nvim_create_autocmd('LspDetach', {
+      group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+      callback = function(event2)
+        vim.lsp.buf.clear_references()
+        vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+      end,
+    })
+  end
+
+  if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+    map('<leader>th', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+    end, 'Toggle Inlay Hints')
+  end
+
+  if client.name == 'jdtls' then
+    map('<leader>dH', ':JdtUpdateHotcode<CR>', 'Hotswap Java Code')
+
+    local jdtls = require 'jdtls'
+
+    map('gs', function()
+      jdtls.super_implementation()
+    end, 'Go to super implementation')
+
+    map('<leader>co', function()
+      jdtls.organize_imports()
+    end, 'Organize Imports')
+
+    map('<leader>cv', function()
+      jdtls.extract_variable()
+    end, 'Extract Variable')
+
+    map('<leader>cc', function()
+      jdtls.extract_constant()
+    end, 'Extract Constant')
+
+    map('<leader>cm', function()
+      jdtls.extract_method()
+    end, 'Extract Method')
+
+    map('<leader>dt', function()
+      jdtls.test_nearest_method()
+    end, 'Debug Test Method')
+
+    map('<leader>dT', function()
+      jdtls.test_class()
+    end, 'Debug Test Class')
+
+    map('<leader>sO', function()
+      jdtls.extended_symbols()
+    end, 'Extended Outline')
+
+    map('<leader>cb', ':JdtCompile<CR>', 'Build Java')
+  elseif client.name == 'clangd' then
+    map('gh', ':LspClangdSwitchSourceHeader<CR>', 'Switch Between Header and Source file')
+  end
+end
+
 return {
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -33,102 +130,7 @@ return {
     config = function()
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
-          end
-
-          local refactor = function(func)
-            return function()
-              nick.utils.before_refactoring()
-              func()
-            end
-          end
-
-          map('<leader>cr', refactor(vim.lsp.buf.rename), 'Rename')
-
-          map('<leader>ca', refactor(vim.lsp.buf.code_action), 'Code Action', { 'n', 'x' })
-
-          map('<leader>ce', vim.diagnostic.open_float, 'Show full diagnostic error message')
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-          if not client then
-            return
-          end
-
-          if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-
-          if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, 'Toggle Inlay Hints')
-          end
-
-          if client.name == 'jdtls' then
-            map('<leader>dH', ':JdtUpdateHotcode<CR>', 'Hotswap Java Code')
-
-            local jdtls = require 'jdtls'
-
-            map('gs', function()
-              jdtls.super_implementation()
-            end, 'Go to super implementation')
-
-            map('<leader>co', function()
-              jdtls.organize_imports()
-            end, 'Organize Imports')
-
-            map('<leader>cv', function()
-              jdtls.extract_variable()
-            end, 'Extract Variable')
-
-            map('<leader>cc', function()
-              jdtls.extract_constant()
-            end, 'Extract Constant')
-
-            map('<leader>cm', function()
-              jdtls.extract_method()
-            end, 'Extract Method')
-
-            map('<leader>dt', function()
-              jdtls.test_nearest_method()
-            end, 'Debug Test Method')
-
-            map('<leader>dT', function()
-              jdtls.test_class()
-            end, 'Debug Test Class')
-
-            map('<leader>sO', function()
-              jdtls.extended_symbols()
-            end, 'Extended Outline')
-
-            map('<leader>cb', ':JdtCompile<CR>', 'Build Java')
-          elseif client.name == 'clangd' then
-            map('gh', ':LspClangdSwitchSourceHeader<CR>', 'Switch Between Header and Source file')
-          end
-        end,
+        callback = on_lsp_attach,
       })
 
       -- Diagnostic Config
@@ -175,8 +177,9 @@ return {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                globals = { 'vim' },
+              },
             },
           },
         },
@@ -188,17 +191,10 @@ return {
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      require('mason-lspconfig').setup {
-        ensure_installed = {},
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+      for server_name, server_config in pairs(servers) do
+        vim.lsp.config(server_name, server_config)
+        vim.lsp.enable(server_name)
+      end
     end,
   },
 }
