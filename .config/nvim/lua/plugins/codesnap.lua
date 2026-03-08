e96c373ecdef310e1d233e0544aa5abc0a6e27b7
Nicholas Allen <nick@bitwig.com>
2026-03-08 20:10:10 +0100

    Disable CodeSnap for now as it breaks blink.cmp

--- .config/nvim/lua/plugins/codesnap.lua
+++ .config/nvim/lua/plugins/codesnap.lua
@@ -1,1 +1,7 @@
-return { 'mistricky/codesnap.nvim', tag = 'v2.0.0' }
+return {
+  'mistricky/codesnap.nvim',
+  tag = 'v2.0.0',
+
+  -- Currently it breaks blink.cmp by overwriting package.cpath so disabled for now
+  enabled = false,
+}
