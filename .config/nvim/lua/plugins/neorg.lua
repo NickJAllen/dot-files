bfc7d0eecd964d33e9bedf849f88c23441469956
Nicholas Allen <radicalnick@gmail.com>
2026-04-24 07:39:11 +0200

    Neorg config

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,3 +1,9 @@
+local indent_value = 2
+
+local function key(k, command, desc)
+  vim.keymap.set('n', k, ':Neorg ' .. command .. '<CR>', { desc = desc, silent = true })
+end
+
 return {
   'nvim-neorg/neorg',
   lazy = false,
@@ -12,7 +18,26 @@ return {
     require('neorg').setup {
       load = {
         ['core.defaults'] = {},
-        ['core.concealer'] = {},
+        ['core.esupports.indent'] = {
+          config = {
+            indents = {
+              _ = { indent = indent_value },
+              heading1 = { indent = 0 * indent_value },
+              heading2 = { indent = 1 * indent_value },
+              heading3 = { indent = 2 * indent_value },
+              heading4 = { indent = 3 * indent_value },
+              heading5 = { indent = 4 * indent_value },
+              heading6 = { indent = 5 * indent_value },
+              paragraph_segment = { indent = indent_value },
+              ranged_tag = { indent = indent_value },
+              ranged_tag_content = { indent = indent_value },
+              strong_paragraph_delimiter = { indent = indent_value },
+            },
+          },
+        },
+        ['core.concealer'] = {
+          indent_multiline_whitespace = true,
+        },
         ['core.dirman'] = {
           config = {
             workspaces = {
@@ -27,7 +52,8 @@ return {
     vim.wo.foldlevel = 99
     vim.wo.conceallevel = 2
 
-    vim.keymap.set('n', '<leader>ni', ':Neorg index<CR>', { desc = 'Neorg Index', silent = true })
-    vim.keymap.set('n', '<leader>nr', ':Neorg return<CR>', { desc = 'Neorg Return', silent = true })
+    key('<leader>ni', 'index', 'Index')
+    key('<leader>nr', 'return', 'Return')
+    key('<leader>nt', 'toggle-concealer', 'Toggle Concealer')
   end,
 }
