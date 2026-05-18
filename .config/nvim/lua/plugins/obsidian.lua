0b5a8d1a660a4ef3dd8f8263be8e01a79795b3df
Nicholas Allen <radicalnick@gmail.com>
2026-05-18 19:14:46 +0200

    Add obsidian plugin and remove neorg

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,30 @@
+local function key(k, action, desc)
+  return { '<leader>n' .. k, ':Obsidian ' .. action .. '<CR>', { desc = desc, silent = true } }
+end
+
+return {
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in 4.0.0
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/obsidian/personal',
+      },
+      {
+        name = 'work',
+        path = '~/obsidian/work',
+      },
+    },
+  },
+  cmd = 'Obsidian',
+  keys = {
+    key('n', 'new', 'New note'),
+    key('s', 'search', 'Search for note'),
+    key('t', 'tags', 'Tags'),
+    key('w', 'workspace', 'Select workspace'),
+  },
+}
