eb0c96a2b27fdd1949b5da5ee27c0d96665a7f55
Nicholas Allen <radicalnick@gmail.com>
2026-09-05 09:42:49 +0200

    Use audio file name for the generated video name

--- .config/fish/functions/music_video.fish
+++ .config/fish/functions/music_video.fish
@@ -3,11 +3,12 @@ function music_video --description "Generates a video for YouTube / Instagram fr
 
     set -l audio_file $argv[1]
     set -l title ""
+    set -l audio_name (basename "$audio_file" | string replace -r '\.[^.]+$' '')
 
     if set -q _flag_title 
         set title $_flag_title
     else
-        set title (basename "$audio_file" | string replace -r '\.[^.]+$' '')
+        set title $audio_name
     end
 
     # Validate inputs
@@ -18,8 +19,8 @@ function music_video --description "Generates a video for YouTube / Instagram fr
     end
 
     set -l image_file "$HOME/Documents/Guitar/Characture.jpeg"
-    set -l titled_image_file "$HOME/Documents/Guitar/Videos/$title.jpeg"
-    set -l output_video "$HOME/Documents/Guitar/Videos/$title.mp4"
+    set -l titled_image_file "$HOME/Documents/Guitar/Videos/$audio_name.jpeg"
+    set -l output_video "$HOME/Documents/Guitar/Videos/$audio_name.mp4"
 
     add_title "$image_file" "$title" "$titled_image_file"
 
